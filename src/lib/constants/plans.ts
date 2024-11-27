235aeb29d848aef580e51cce187d11fef26b48f4
kokornika <160030659+kokornika@users.noreply.github.com>
2024-11-27 21:12:47 +0100

    Add files via upload

--- src/lib/constants/plans.ts
+++ src/lib/constants/plans.ts
@@ -10,7 +10,7 @@ export const PAYMENT_PLANS: PaymentPlan[] = [
   {
     id: 'basic',
     name: 'Alap csomag',
-    price: 9900,
+    price: 500,
     period: '3 hónap',
     features: [
       'Egyedi digitális névjegykártya',
