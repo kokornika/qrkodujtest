1fbe43aac13f67b4d4941cd35766a8e8447ccfb6
kokornika <160030659+kokornika@users.noreply.github.com>
2024-12-01 15:34:48 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,16 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          qr: ['qrcode', 'qrcode.react'],
+        },
+      },
+    },
+  },
   optimizeDeps: {
     include: ['qrcode', 'qrcode.react'],
   },
@@ -21,4 +31,9 @@ export default defineConfig({
       usePolling: true,
     },
   },
+  define: {
+    'process.env.VITE_EMAIL_SERVICE_ID': JSON.stringify(''),
+    'process.env.VITE_EMAIL_TEMPLATE_ID': JSON.stringify(''),
+    'process.env.VITE_EMAIL_USER_ID': JSON.stringify(''),
+  }
 });
